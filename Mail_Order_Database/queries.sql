e8d9a2b5d1ce37da127b6dbba9fb5375f425d1c8
manmanmano <jimiskools@gmail.com>
2021-11-20 17:17:25 +0200

    done

--- Mail_Order_Database/queries.sql
+++ Mail_Order_Database/queries.sql
@@ -1,21 +1,45 @@
+-- -----------------------------------------------------
+-- FIRST QUERY	
+-- -----------------------------------------------------
+
 /*
 Query orders in a specified time frame returning:
 - order number
-- customer name
+- customer name and ID
 - number of ordered items
 - total sum
 - expected shipping
 - actual shipping 
 */
 
-SELECT SUM(Order_part.Quantity_in_cart * Part.Price) AS Total, 
-SUM(Order_part.Quantity_in_cart) AS Ordered_items, Order_list.Order_ID, 
-Customer.First_name AS Customer_name,  Order_list.Actual_ship, Order_list.Planned_ship 
-FROM Customer
-INNER JOIN Order_list ON Order_list.Customer_ID = Customer.Customer_ID
-INNER JOIN Order_part ON Order_part.Order_ID = Order_list.Order_ID
-INNER JOIN Part ON Part.Part_ID = Order_part.Part_ID
-WHERE Order_list.Date_of_receipt BETWEEN '2021-01-01' AND '2021-12-31'
-GROUP BY 
-Order_list.Order_ID, Customer.First_name, Order_list.Actual_ship, Order_list.Planned_ship
-ORDER BY Order_list.Order_ID ASC;
+SELECT SUM(OP.Quantity_in_cart * P.Price) AS Total, SUM(OP.Quantity_in_cart) AS Ordered_quantities, 
+COUNT(OP.Part_ID) AS Ordered_items, CONCAT(C.First_name, ' ', C.Last_name) AS Customer_fullname,
+OL.Order_ID, C.Customer_ID,  OL.Actual_ship, OL.Planned_ship 
+FROM Customer C
+INNER JOIN Order_list OL ON OL.Customer_ID = C.Customer_ID
+INNER JOIN Order_part OP ON OP.Order_ID = OL.Order_ID
+INNER JOIN Part P ON P.Part_ID = OP.Part_ID
+WHERE OL.Date_of_receipt BETWEEN '2021-01-01' AND '2021-12-31'
+GROUP BY OL.Order_ID, C.Customer_ID, C.First_name, OL.Actual_ship, OL.Planned_ship
+ORDER BY OL.Order_ID ASC;
+
+
+-- -----------------------------------------------------
+-- SECOND QUERY
+-- -----------------------------------------------------
+
+/*
+Select a single order and list:
+- order header info (client name and address)
+- ordered items
+- ordered quantities
+*/
+
+SELECT SUM(OP.Quantity_in_cart) AS Ordered_quantities, CONCAT(C.First_name, ' ', C.Last_name) AS Customer_fullname,
+C.Customer_ID, C.ZIP_code AS Customer_ZIP, OL.Order_ID, OP.Quantity_in_cart, P.Part_name
+FROM Part P
+INNER JOIN Order_part OP ON OP.Part_ID = P.Part_ID
+INNER JOIN Order_list OL ON OL.Order_ID = OP.Order_ID
+INNER JOIN Customer C ON C.Customer_ID = OL.Customer_ID
+WHERE OL.Order_ID = '2'
+GROUP BY OL.Order_ID, OP.Quantity_in_cart, P.Part_name, C.Customer_ID, Customer_ZIP, Customer_fullname;
